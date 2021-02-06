bca8c3710ed5e75b2bcd19af31f4ccb5adee463e
dev-shashank <shashank.professional.96@gmail.com>
2021-02-06 15:58:34 +0530

    Added login route and user verification

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,4 +45,31 @@ router.post('/register', (req, res) => {
         })
 });
 
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public route
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({email})
+        .then(user => {
+            // Check for user
+            if(!user) {
+                return res.status(400).json({ email: 'User not found' });
+            }
+
+            // Check Password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({ msg: 'Success'})
+                    } else {
+                        return res.status(400).json({ password: 'Password incorrect' })
+                    }
+                })
+        })
+});
+
 module.exports = router;
