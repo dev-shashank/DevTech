a75e9446af118c80ae24b74984648bf0ef566190
dev-shashank <shashank.professional.96@gmail.com>
2021-02-07 23:05:25 +0530

    Added React component for Landing page

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
